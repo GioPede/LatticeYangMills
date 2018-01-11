6308a669e5478c571a7fac2bbae49016254274fd
GioPede <giovanni.pederiva@gmail.com>
2018-01-11 11:43:36 +0100

    Doxygen test

---
+++ include/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*! \brief Brief description.
+ *         Brief description continued.
+ * 
+ *  Detailed description starts here.
+ * 
+ */
+
+int main(){
+	std::cout << "Hi!" << std::endl;
+	return 0;
+}
